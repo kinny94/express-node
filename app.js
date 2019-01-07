83d29c7066e201ae9e894533ce53284859b5c069
Arjun Dass <ad.sam007@gmail.com>
2019-01-07 12:54:51 -0500

    feat: add express routes

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 const app =  express();
 
 app.use(bodyParser.urlencoded({extended: false}));
-
-app.use('/', (req, res, next) => {
-    console.log("In another middleware");
-});
+app.use(shopRoutes);
+app.use(adminRoutes);
 
 app.listen(3000);
